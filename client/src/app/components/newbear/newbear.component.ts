1ff1cddef7883ce8506ae2b9ef97aab43512e4e4
xanukum <xanukum@gmail.com>
2018-10-31 18:35:29 +0530

    new build

--- client/src/app/components/newbear/newbear.component.ts
+++ client/src/app/components/newbear/newbear.component.ts
@@ -3,7 +3,13 @@ import {MatChipInputEvent} from '@angular/material';
 import { ActivatedRoute } from '@angular/router';
 import {COMMA, ENTER} from '@angular/cdk/keycodes';
 import { NgForm } from '@angular/forms';
-
+import { MatDialog } from '@angular/material';
+import { UIService } from '../../shared/snackbar/Ui.service';
+import { AuthService } from '../../auth/auth.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BeerService } from '../../services/beer.service';
+import { Subscription } from 'rxjs';
+import { Dialogbox } from '../../dialogbox/dialog.component';
 
 export interface Fruit {
   name: string;
@@ -20,7 +26,16 @@ export class NewbearComponent implements OnInit {
   selectable = true;
   removable = true;
   addOnBlur = true;
-
+  isLoading: Boolean;
+  info: any;
+  status: any;
+  isloadsub: Subscription;
+  authSubscription: Subscription;
+  issubscription_expired: boolean;
+  issubscription: boolean;
+  isAuth: Boolean = false;
+  userdata: any;
+  location_id: any;
   price: String= '';
 
   density: String= '';
@@ -32,7 +47,10 @@ export class NewbearComponent implements OnInit {
 
   beer_description: String= '';
 
+  filelink: any;
 
+  imageis: any;
+x = [1];
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
   fruits: Fruit[] = [
     {name: 'Lemon'},
@@ -63,18 +81,84 @@ export class NewbearComponent implements OnInit {
     }
   }
 
-  constructor() {
+  constructor(               private authService: AuthService,
+    private beerService: BeerService,
+    private spinner: NgxSpinnerService,  private uiservice: UIService,
+    public dialog: MatDialog
+  ) {
 
    }
 
   ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(resp => {
+      this.isAuth = resp.isauth;
+      this.issubscription_expired = resp.issubscription_expired;
+      this.issubscription = resp.issubscription;
+    });
 
+    this.isAuth = this.authService.isLoggedIn();
+    if ( this.isAuth ) {
+      this.userdata = JSON.parse(localStorage.getItem('user'));
+      this.location_id = this.userdata.location.id;
+    }
 }
 
 
+imageUpload(e) {
+  console.log('sasd');
+  const reader = new FileReader();
+  const file = e.target.files[0];
+  if (file) {
+       this.filelink  = file;
+    }
+}
+openDialog(): void {
+  const dialogRef = this.dialog.open(Dialogbox, {
+    width: '350px',
+    data: { text: this.info, status: this.status }
+  });
+
+  dialogRef.afterClosed().subscribe(result => {
+    console.log('The dialog was closed' + result);
+  });
+}
   addBear(form: NgForm) {
           const val = form.value;
       console.log('sadsadsad', val);
+      console.log('filelink', this.filelink);
+     if (this.filelink) {
+             console.log('console', this.filelink);
+             const formData: any = new FormData();
+             formData.append('beer_logo', this.filelink);
+             formData.append('name', this.name);
+             formData.append('price', this.price);
+             formData.append('alchohol_content', this.alchohol_content);
+             formData.append('beer_description', this.beer_description);
+             formData.append('location_id', 3);
+             formData.append('category', this.x);
+
+             const headers = new Headers();
+             this.beerService.addaBear( formData, this.location_id ).subscribe(res => {
+               if (res.error === false) {
+                   this.info = res.text;
+                   this.status = false;
+                //   form.reset();
+                   this.openDialog();
+                 } else {
+                   this.info = res.text;
+                   this.status = true;
+                   this.uiservice.showsnackbar(res.text, null, 3000);
+                 }
+               }, error => {
+                 this.info = error;
+                 this.status = false;
+                 this.uiservice.showsnackbar(error.message, null, 3000);
+               });
+      } else {
+        console.log('s');
+        this.uiservice.showsnackbar( 'Plaese select an image', null, 3000);
+
+      }
 
 }
 
--- client/src/app/services/beer.service.ts
+++ client/src/app/services/beer.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse, } from '@angular/common/http';
 import { Beer } from '../models/beer.model';
 import { ServerResponseArray } from '../models/server-request-array.model';
 import { Observable } from 'rxjs/Observable';
 import { BreweryInfo } from '../services/beweryinfo.service';
+import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class BeerService {
@@ -29,4 +31,14 @@ export class BeerService {
 		console.log('beer id is ', location_id);
 		return this.http.get(`/api/beer/${location_id}`).map(res => res);
 	}
+
+
+	addaBear(formData, location_id): Observable<any> {
+		const header = new HttpHeaders();
+		return this.http.post<any>(`/api/beer/location`, formData,
+			{ observe: 'response', responseType: 'json', headers: header })
+			 .pipe(map((res) => { console.log(res.body); return res.body; }));
+	}
+
+
 }
